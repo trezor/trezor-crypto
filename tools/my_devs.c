9252479ddec64c90c148ea5ed70a3045e5eedee5
mpsido <mpsido2@gmail.com>
2018-02-07 16:24:18 +0800

    Convert to bigint and try to compute bn_mod, but still not the same result as cipher's EDCH

--- tools/my_devs.c
+++ tools/my_devs.c
@@ -7,6 +7,8 @@
 #include "bip32.h"
 #include "curves.h"
 
+#include "bignum.h"
+
 void compute_sha256sum(const char *seed, uint8_t* digest /*size SHA256_DIGEST_LENGTH*/);
 
 void generate_shared_key(const char *seed_str) {
@@ -40,19 +42,29 @@ void generate_shared_key(const char *seed_str) {
 
     res = hdnode_get_shared_key(&alice, alice.public_key, session_key1, &key_size);
 
-
-    printf("Shared key (status: %d, key_size: %d): ", res, key_size);
+    printf("Shared key (status: %d, key_size: %d): \n", res, key_size);
     for (int i = 0; i < key_size; ++i)
     {
-    printf("%02x", session_key1[i]);
+        printf("%02x", session_key1[i]);
     }
-    printf("\n");
 
+    bignum256 bigshared;
+#if BYTE_ORDER == LITTLE_ENDIAN
+    bn_read_le(session_key1, &bigshared);
+#elif BYTE_ORDER == BIG_ENDIAN
+    bn_read_be(session_key1, &bigshared);
+#endif
+    printf("\nbignum sharedkey : \n");
+    bn_print(&bigshared);
+
+    printf("\nbignum sharedkey after mod : \n");
+    bn_fast_mod(&bigshared, &alice.curve->params->order);
+    bn_print(&bigshared);
 
     uint8_t digest[SHA256_DIGEST_LENGTH]= {0};
     compute_sha256sum((const char*) session_key1, digest);
     
-    printf("output: \n");
+    printf("\nSha256 of output: \n");
     for(uint i=0;i<SHA256_DIGEST_LENGTH;i++) {
         printf("%02x", digest[i]);
     }
@@ -86,7 +98,7 @@ int main(int argc, char *argv[])
     uint8_t digest[SHA256_DIGEST_LENGTH]= {0};
     compute_sha256sum(seed, digest);
 
-    printf("output: \n");
+    printf("Sha256: \n");
     for(uint i=0;i<SHA256_DIGEST_LENGTH;i++) {
         printf("%02x", digest[i]);
     }
